5eb3ed2cf7cd62543e5108cd6ea42e64a8ee891d
alinrdn <ali.nrdn14005@gmail.com>
2026-06-07 20:55:12 +0700

    update dashboard admin

---
+++ app/Data/AdminMenus.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Data;
+
+class AdminMenus
+{
+    /**
+     * Mengembalikan struktur menu admin.
+     * Digunakan oleh sidebar (menu.blade.php) dan dashboard (dashboard.blade.php).
+     *
+     * Setiap section berisi:
+     *   - header : label section di sidebar
+     *   - groups : daftar grup menu
+     *
+     * Setiap group berisi:
+     *   - title        : nama grup
+     *   - sidebar_icon : icon tabler untuk sidebar parent
+     *   - direct       : true jika langsung link (bukan dropdown)
+     *   - route        : (hanya jika direct=true) route direct
+     *   - items        : sub-item [ route, label, icon (gambar dashboard) ]
+     */
+    public static function sections(): array
+    {
+        return [
+            [
+                'header' => 'Konten Website',
+                'groups' => [
+                    [
+                        'title'        => 'Banner',
+                        'sidebar_icon' => 'ti ti-photo',
+                        'items' => [
+                            ['route' => 'admin.master.banner.index',         'label' => 'Beranda', 'icon' => 'icon-be-19.png'],
+                            ['route' => 'admin.master.banner-halaman.index', 'label' => 'Halaman', 'icon' => 'icon-be-28.png'],
+                        ],
+                    ],
+                    [
+                        'title'        => 'Master Data',
+                        'sidebar_icon' => 'ti ti-database',
+                        'items' => [
+                            ['route' => 'admin.master.bisnis-kategori.index', 'label' => 'Bisnis Kategori',   'icon' => 'icon-be-21.png'],
+                            ['route' => 'admin.master.company.index',         'label' => 'Mengenai Kami',      'icon' => 'icon-be-26.png'],
+                            ['route' => 'admin.master.customer.index',        'label' => 'Mitra Strategis',    'icon' => 'icon-be-10.png'],
+                            ['route' => 'admin.master.pengalaman.index',      'label' => 'Pengalaman Kami',    'icon' => 'icon-be-23.png'],
+                            ['route' => 'admin.master.personil.index',        'label' => 'Personil Manajemen', 'icon' => 'icon-be-22.png'],
+                            ['route' => 'admin.master.iso.index',             'label' => 'Sertifikasi',        'icon' => 'icon-be-17.png'],
+                        ],
+                    ],
+                    [
+                        'title'        => 'Dokumen',
+                        'sidebar_icon' => 'ti ti-files',
+                        'items' => [
+                            ['route' => 'admin.master.dokumen-kategori.index', 'label' => 'Kategori', 'icon' => 'icon-be-16.png'],
+                            ['route' => 'admin.master.dokumen.index',          'label' => 'Dokumen',  'icon' => 'icon-be-27.png'],
+                        ],
+                    ],
+                    [
+                        'title'        => 'Berita',
+                        'sidebar_icon' => 'ti ti-news',
+                        'items' => [
+                            ['route' => 'admin.master.berita-kategori.index', 'label' => 'Kategori', 'icon' => 'icon-be-11.png'],
+                            ['route' => 'admin.master.berita.index',          'label' => 'Berita',   'icon' => 'icon-be-15.png'],
+                        ],
+                    ],
+                ],
+            ],
+
+            [
+                'header' => 'User Management',
+                'groups' => [
+                    [
+                        'title'        => 'Users',
+                        'sidebar_icon' => 'ti ti-users',
+                        'direct'       => true,
+                        'route'        => 'admin.userman.user.view',
+                        'items' => [
+                            ['route' => 'admin.userman.user.view', 'label' => 'Users', 'icon' => 'icon-be-02.png'],
+                        ],
+                    ],
+                    [
+                        'title'        => 'Roles & Permissions',
+                        'sidebar_icon' => 'ti ti-settings',
+                        'items' => [
+                            ['route' => 'admin.userman.role.view',       'label' => 'Roles',       'icon' => 'icon-be-06.png'],
+                            ['route' => 'admin.userman.permission.view', 'label' => 'Permissions', 'icon' => 'icon-be-09.png'],
+                        ],
+                    ],
+                ],
+            ],
+
+            [
+                'header' => 'Settings',
+                'groups' => [
+                    [
+                        'title'        => 'App Settings',
+                        'sidebar_icon' => 'ti ti-table-options',
+                        'direct'       => true,
+                        'route'        => 'admin.setting.appsetting.index',
+                        'items' => [
+                            ['route' => 'admin.setting.appsetting.index', 'label' => 'App Setting', 'icon' => 'icon-be-05.png'],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -1,76 +1,4 @@
-@php
-    $bannerMenus = [
-        [
-            'route' => 'admin.master.banner.index',
-            'title' => 'Home Banner',
-            'icon'  => 'ti ti-home',
-        ],
-        [
-            'route' => 'admin.master.banner-halaman.index',
-            'title' => 'Banner Halaman',
-            'icon'  => 'ti ti-layout',
-        ],
-    ];
-
-    $masterDataMenus = [
-        [
-            'route' => 'admin.master.bisnis-kategori.index',
-            'title' => 'Bisnis Kategori',
-            'icon'  => 'ti ti-category',
-        ],
-        [
-            'route' => 'admin.master.company.index',
-            'title' => 'Mengenai Kami',
-            'icon'  => 'ti ti-building',
-        ],
-        [
-            'route' => 'admin.master.customer.index',
-            'title' => 'Mitra Strategis',
-            'icon'  => 'ti ti-handshake',
-        ],
-        [
-            'route' => 'admin.master.pengalaman.index',
-            'title' => 'Pengalaman Kami',
-            'icon'  => 'ti ti-star',
-        ],
-        [
-            'route' => 'admin.master.personil.index',
-            'title' => 'Personil Manajemen',
-            'icon'  => 'ti ti-users',
-        ],
-        [
-            'route' => 'admin.master.iso.index',
-            'title' => 'Sertifikasi',
-            'icon'  => 'ti ti-certificate',
-        ],
-    ];
-
-    $dokumenMenus = [
-        [
-            'route' => 'admin.master.dokumen-kategori.index',
-            'title' => 'Kategori',
-            'icon'  => 'ti ti-folder',
-        ],
-        [
-            'route' => 'admin.master.dokumen.index',
-            'title' => 'Dokumen',
-            'icon'  => 'ti ti-file',
-        ],
-    ];
-
-    $beritaMenus = [
-        [
-            'route' => 'admin.master.berita-kategori.index',
-            'title' => 'Kategori',
-            'icon'  => 'ti ti-tag',
-        ],
-        [
-            'route' => 'admin.master.berita.index',
-            'title' => 'Berita',
-            'icon'  => 'ti ti-news',
-        ],
-    ];
-@endphp
+@php use App\Data\AdminMenus; @endphp
 
 <ul class="menu-inner py-1">
 
@@ -82,117 +10,52 @@
     </a>
   </li>
 
-  {{-- Section: Databases --}}
-  <li class="menu-header small text-uppercase">
-    <span class="menu-header-text">Konten Website</span>
-  </li>
+  @foreach (AdminMenus::sections() as $section)
 
-  {{-- Banner --}}
-  <li class="menu-item {{ isActiveOpen(['admin.master.banner.*', 'admin.master.banner-halaman.*']) }}">
-    <a href="javascript:void(0);" class="menu-link menu-toggle">
-      <i class="menu-icon tf-icons ti ti-photo"></i>
-      <div>Banner</div>
-    </a>
-    <ul class="menu-sub">
-      @foreach ($bannerMenus as $item)
-        <li class="menu-item {{ isActiveRoute($item['route']) }}">
-          <a href="{{ route($item['route']) }}" class="menu-link">
-            <div>{{ $item['title'] }}</div>
+    {{-- Section Header --}}
+    <li class="menu-header small text-uppercase">
+      <span class="menu-header-text">{{ $section['header'] }}</span>
+    </li>
+
+    @foreach ($section['groups'] as $group)
+
+      @if (!empty($group['direct']))
+        {{-- Direct link (tidak dropdown) --}}
+        <li class="menu-item {{ isActiveRoute($group['route']) }}">
+          <a href="{{ route($group['route']) }}" class="menu-link">
+            <i class="menu-icon tf-icons {{ $group['sidebar_icon'] }}"></i>
+            <div>{{ $group['title'] }}</div>
           </a>
         </li>
-      @endforeach
-    </ul>
-  </li>
 
-  {{-- Master Data --}}
-  <li class="menu-item {{ isActiveOpen(['admin.master.bisnis-kategori.*', 'admin.master.company.*', 'admin.master.customer.*', 'admin.master.pengalaman.*', 'admin.master.personil.*', 'admin.master.iso.*']) }}">
-    <a href="javascript:void(0);" class="menu-link menu-toggle">
-      <i class="menu-icon tf-icons ti ti-database"></i>
-      <div>Master Data</div>
-    </a>
-    <ul class="menu-sub">
-      @foreach ($masterDataMenus as $item)
-        <li class="menu-item {{ isActiveRoute($item['route']) }}">
-          <a href="{{ route($item['route']) }}" class="menu-link">
-            <div>{{ $item['title'] }}</div>
+      @else
+        {{-- Dropdown --}}
+        @php
+          // Konversi 'admin.master.banner.index' → 'admin.master.banner.*'
+          $groupRoutes = array_map(
+              fn($item) => preg_replace('/\.[^.]+$/', '.*', $item['route']),
+              $group['items']
+          );
+        @endphp
+        <li class="menu-item {{ isActiveOpen($groupRoutes) }}">
+          <a href="javascript:void(0);" class="menu-link menu-toggle">
+            <i class="menu-icon tf-icons {{ $group['sidebar_icon'] }}"></i>
+            <div>{{ $group['title'] }}</div>
           </a>
+          <ul class="menu-sub">
+            @foreach ($group['items'] as $item)
+              <li class="menu-item {{ isActiveRoute($item['route']) }}">
+                <a href="{{ route($item['route']) }}" class="menu-link">
+                  <div>{{ $item['label'] }}</div>
+                </a>
+              </li>
+            @endforeach
+          </ul>
         </li>
-      @endforeach
-    </ul>
-  </li>
+      @endif
 
-  {{-- Dokumen --}}
-  <li class="menu-item {{ isActiveOpen(['admin.master.dokumen-kategori.*', 'admin.master.dokumen.*']) }}">
-    <a href="javascript:void(0);" class="menu-link menu-toggle">
-      <i class="menu-icon tf-icons ti ti-files"></i>
-      <div>Dokumen</div>
-    </a>
-    <ul class="menu-sub">
-      @foreach ($dokumenMenus as $item)
-        <li class="menu-item {{ isActiveRoute($item['route']) }}">
-          <a href="{{ route($item['route']) }}" class="menu-link">
-            <div>{{ $item['title'] }}</div>
-          </a>
-        </li>
-      @endforeach
-    </ul>
-  </li>
+    @endforeach
 
-  {{-- Berita --}}
-  <li class="menu-item {{ isActiveOpen(['admin.master.berita-kategori.*', 'admin.master.berita.*']) }}">
-    <a href="javascript:void(0);" class="menu-link menu-toggle">
-      <i class="menu-icon tf-icons ti ti-news"></i>
-      <div>Berita</div>
-    </a>
-    <ul class="menu-sub">
-      @foreach ($beritaMenus as $item)
-        <li class="menu-item {{ isActiveRoute($item['route']) }}">
-          <a href="{{ route($item['route']) }}" class="menu-link">
-            <div>{{ $item['title'] }}</div>
-          </a>
-        </li>
-      @endforeach
-    </ul>
-  </li>
-
-  {{-- User Management --}}
-  <li class="menu-header small text-uppercase">
-    <span class="menu-header-text">User Management</span>
-  </li>
-  <li class="menu-item {{ isActiveRoute('admin.userman.user.view') }}">
-    <a href="{{ route('admin.userman.user.view') }}" class="menu-link">
-      <i class="menu-icon tf-icons ti ti-users"></i>
-      <div>Users</div>
-    </a>
-  </li>
-  <li class="menu-item {{ isActiveOpen(['admin.userman.role.*', 'admin.userman.permission.*']) }}">
-    <a href="javascript:void(0);" class="menu-link menu-toggle">
-      <i class="menu-icon tf-icons ti ti-settings"></i>
-      <div>Roles & Permissions</div>
-    </a>
-    <ul class="menu-sub">
-      <li class="menu-item {{ isActiveRoute('admin.userman.role.view') }}">
-        <a href="{{ route('admin.userman.role.view') }}" class="menu-link">
-          <div>Roles</div>
-        </a>
-      </li>
-      <li class="menu-item {{ isActiveRoute('admin.userman.permission.view') }}">
-        <a href="{{ route('admin.userman.permission.view') }}" class="menu-link">
-          <div>Permission</div>
-        </a>
-      </li>
-    </ul>
-  </li>
-
-  {{-- Settings --}}
-  <li class="menu-header small text-uppercase">
-    <span class="menu-header-text">Settings</span>
-  </li>
-  <li class="menu-item {{ isActiveRoute('admin.setting.appsetting.index') }}">
-    <a href="{{ route('admin.setting.appsetting.index') }}" class="menu-link">
-      <i class="menu-icon tf-icons ti ti-table-options"></i>
-      <div>App Settings</div>
-    </a>
-  </li>
+  @endforeach
 
 </ul>
